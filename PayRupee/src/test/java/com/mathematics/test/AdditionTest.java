dea4f29aa881b1a6f52f83d9fdca53566b0cbc05
HP <HP@ARBAZ>
2023-02-11 11:33:02 +0530

    Latest Commit

---
+++ PayRupee/src/test/java/com/mathematics/test/AdditionTest.java
@@ -0,0 +1,14 @@
+package com.mathematics.test;
+
+import org.testng.annotations.Test;
+
+@Test
+public class AdditionTest {
+
+	public void main() {
+		int a = 2;
+		int b = 3;		
+		System.out.println("===Addition===== : "+(a+b));
+	}
+
+}
---
+++ PayRupee/src/test/java/com/mathematics/test/SubstractionTest.java
@@ -0,0 +1,13 @@
+package com.mathematics.test;
+
+import org.testng.annotations.Test;
+
+@Test
+public class SubstractionTest {
+
+	public void main() {
+		int a = 5;
+		int b = 3;		
+		System.out.println("===Substraction===== : "+(a-b));
+	}
+}
